a16b308521b81f61093779cc38ca9e8d0a835893
Vanessa Williams <vanessa@fridgebuzz.com>
2010-03-30 19:12:47 +0000

    JavaDoc. Calculate mean and standard deviation of estimated insert sizes. Added method to determine and set the read pair type. Added function to infer insert sizes.
    git-svn-id: https://svn2.assembla.com/svn/savant/Savant/trunk@407 096565f4-0d05-4fcd-8cbf-ff6523290f6f

--- src/savant/model/data/interval/BAMIntervalTrack.java
+++ src/savant/model/data/interval/BAMIntervalTrack.java
@@ -30,6 +30,7 @@ import savant.model.BAMIntervalRecord;
 import savant.model.Resolution;
 import savant.model.data.RecordTrack;
 import savant.util.Range;
+import savant.view.swing.Savant;
 
 import java.io.File;
 import java.util.ArrayList;
@@ -49,10 +50,18 @@ public class BAMIntervalTrack implements RecordTrack<BAMIntervalRecord> {
     private SAMFileReader samFileReader;
     private String sequenceName;
 
+    private float mean;
+    private float stdDeviation;
+
     public BAMIntervalTrack(File path, File index) {
         setPath(path, index);
+        guessSequence();
+        calculateStatistics();
     }
 
+    /**
+     * {@inheritDoc}
+     */
     public List<BAMIntervalRecord> getRecords(Range range, Resolution resolution) {
 
         CloseableIterator<SAMRecord> recordIterator=null;
@@ -69,40 +78,60 @@ public class BAMIntervalTrack implements RecordTrack<BAMIntervalRecord> {
                 bamRecord = new BAMIntervalRecord(samRecord);
 
                 // find out the type of the pair
-                BAMIntervalRecord.PairType type = setPairType(samRecord.getAlignmentStart(), samRecord.getMateAlignmentStart(), samRecord.getReadNegativeStrandFlag(), samRecord.getMateNegativeStrandFlag(), bamRecord);
+                BAMIntervalRecord.PairType type = getPairType(samRecord.getAlignmentStart(), samRecord.getMateAlignmentStart(), samRecord.getReadNegativeStrandFlag(), samRecord.getMateNegativeStrandFlag());
+                bamRecord.setType(type);
 
-                // if it's one of the types where we need to know the mate's alignmnent end, record it in a separate list
-                if (type == BAMIntervalRecord.PairType.INVERTED_MATE || type == BAMIntervalRecord.PairType.EVERTED) {
-                    invertedMateOrEverted.add(bamRecord);
-                }
                 result.add(bamRecord);
             }
         } finally {
             if (recordIterator != null) recordIterator.close();
         }
 
-        // get the mate records too, as we'll need them
-        SAMRecord mateRecord;
-        for (BAMIntervalRecord bamRecord : invertedMateOrEverted) {
-            if (bamRecord.getSamRecord().getFirstOfPairFlag()) {
-                mateRecord = samFileReader.queryMate(bamRecord.getSamRecord());
-                bamRecord.setMateRecord(mateRecord);
-            }
-        }
         return result;
     }
 
+    /**
+     * Get the path file of this data track
+     *
+     * @return path
+     */
     public File getPath() {
         return path;
     }
 
-    public void setPath(File path, File index) {
+    /**
+     * Get mean of estimated insert sizes.
+     *
+     * @return mean of insert sizes
+     */
+    public float getMean() {
+        return mean;
+    }
+
+    /**
+     * Get standard deviation of estimated insert sizes.
+     *
+     * @return std dev of insert sizes
+     */
+    public float getStdDeviation() {
+        return stdDeviation;
+    }
+
+    private void setPath(File path, File index) {
 
         if (path == null) throw new IllegalArgumentException("File must not be null.");
         if (index == null) throw new IllegalArgumentException("Index file must not be null");
         this.path = path;
         this.index = index;
         samFileReader = new SAMFileReader(path, index);
+
+    }
+
+    /*
+     * Use the length of the reference genome to guess which sequence from the dictionary
+     * we should search for reads.
+     */
+    private void guessSequence() {
 
         // Find out what sequence we're using, by reading the header for sequences and lengths
         RangeController rangeController = RangeController.getInstance();
@@ -125,11 +154,109 @@ public class BAMIntervalTrack implements RecordTrack<BAMIntervalRecord> {
         if (closestSequenceIndex != Integer.MAX_VALUE) {
             this.sequenceName = sequenceDictionary.getSequence(closestSequenceIndex).getSequenceName();
         }
+
     }
 
-    private BAMIntervalRecord.PairType setPairType(int readStart, int mateStart, boolean readNegative, boolean mateNegative, BAMIntervalRecord record) {
+    /*
+     * Calculate the mean and standard deviation of normal reads.
+     */
+    private void calculateStatistics() {
+        CloseableIterator<SAMRecord> recordIterator=null;
+        try {
+            recordIterator = samFileReader.query(sequenceName, 2000, 4000, false);
+            SAMRecord samRecord;
 
+            int n = 0;
+            float mean = 0.0f;
+            float m2 = 0.0f;
+            
+            while (recordIterator.hasNext()) {
+
+                // get next record
+                samRecord = recordIterator.next();
+
+                // throw out unmapped and unpaired reads
+                if (samRecord.getReadUnmappedFlag() || !samRecord.getReadPairedFlag() || samRecord.getMateUnmappedFlag()) continue;
+
+                // only use the left-most of the pair
+                if (samRecord.getAlignmentStart() > samRecord.getMateAlignmentStart() ) continue;
+
+                // only use NORMAL pair types
+                BAMIntervalRecord.PairType pairType = getPairType(samRecord.getAlignmentStart(), samRecord.getMateAlignmentStart(),
+                        samRecord.getReadNegativeStrandFlag(), samRecord.getMateNegativeStrandFlag());
+                if (pairType != BAMIntervalRecord.PairType.NORMAL) continue; // don't want crazy numbers blowing our statistics
+
+                n++;
+                int x = inferInsertSize(samRecord, pairType);
+                if (x > 2000) Savant.log("insert size > 2000 in statistics calculation");
+                float delta = x - mean;
+                mean = mean + delta/n;
+                m2 = m2 + delta*(x - mean);
+            }
+            float variance = m2/(n-1);
+            this.mean = mean;
+            this.stdDeviation = (float)Math.sqrt((double) variance);
+//            Savant.log("Mean and Standard Deviation are: " + this.mean + " , " + this.stdDeviation);
+        } finally {
+            if (recordIterator != null) recordIterator.close();
+        }
+    }
+
+    /**
+     * Calculate approximate insert size for a given mated pair of reads. Some assumptions are
+     * made about the equivalence of read and mate lengths, since there is no other reasonable
+     * heuristic that works across sequencing technologies.
+     *
+     * @param samRecord a SAMRecord returned by SAMTools
+     * @param pairType the pair type: NORMAL, INVERTED_READ, INVERTED_MATE, EVERTED
+     * @return an approximate insert size
+     * @throws IllegalStateException if the read is unmapped, unpaired, or its mate is unmapped.
+     */
+    public static int inferInsertSize(SAMRecord samRecord, BAMIntervalRecord.PairType pairType) throws IllegalStateException {
+
+        if (samRecord.getReadUnmappedFlag() || !samRecord.getReadPairedFlag() || samRecord.getMateUnmappedFlag())
+            throw new IllegalStateException("Read is either not mapped, not paired, or its mate is unmapped; cannot calculate insert size.");
+
+        int startPos = 0;
+        int endPos = 0;
+
+        int alignmentStart = samRecord.getAlignmentStart();
+        int alignmentEnd = samRecord.getAlignmentEnd();
+        int mateAlignmentStart = samRecord.getMateAlignmentStart();
+        // this is an approximation
+        int mateAlignmentEnd = samRecord.getMateAlignmentStart() + samRecord.getReadLength();
+
+
+
+        switch (pairType) {
+            case NORMAL:
+                startPos = alignmentEnd;
+                endPos = mateAlignmentStart;
+                break;
+            case INVERTED_READ:
+                startPos = alignmentStart;
+                endPos = mateAlignmentStart;
+                break;
+            case INVERTED_MATE:
+                startPos = alignmentEnd;
+                endPos = mateAlignmentEnd;
+                break;
+            case EVERTED:
+                startPos = alignmentStart;
+                endPos = mateAlignmentEnd;
+                break;
+        }
+        return endPos - startPos + 1;
+    }
+
+    /*
+     * Determine the pair type: NORMAL, INVERTED_READ, INVERTED_MATE, EVERTED pair.
+     */
+    private BAMIntervalRecord.PairType getPairType(int readStart, int mateStart, boolean readNegative, boolean mateNegative) {
+
+        BAMIntervalRecord.PairType type=null;
         boolean readNegativeStrand, mateNegativeStrand;
+
         // by switching the negative strand flags based on which read comes first, we can reduce 8 cases to 4
         if (readStart < mateStart) {
             readNegativeStrand = readNegative;
@@ -143,30 +270,38 @@ public class BAMIntervalTrack implements RecordTrack<BAMIntervalRecord> {
         // now is the first read pointing forward & mate pointing backward?
         if (!readNegativeStrand && mateNegativeStrand) {
             // congratulations, it's a normal pair!
-            record.setType(BAMIntervalRecord.PairType.NORMAL);
+            type = BAMIntervalRecord.PairType.NORMAL;
         }
         // or are both reversed?
         else if (readNegativeStrand && mateNegativeStrand) {
             // this is a case of the read being inverted
-            record.setType(BAMIntervalRecord.PairType.INVERTED_READ);
+            type = BAMIntervalRecord.PairType.INVERTED_READ;
         }
         // or are both forward?
         else if (!readNegativeStrand && !mateNegativeStrand) {
             // this is a case of the mate being inverted
-            record.setType(BAMIntervalRecord.PairType.INVERTED_MATE);
+            type = BAMIntervalRecord.PairType.INVERTED_MATE;
         }
         // are the strands pointing away from each other?
         else if (readNegativeStrand && !mateNegativeStrand) {
             // the pair is everted
-            record.setType(BAMIntervalRecord.PairType.EVERTED);
+            type = BAMIntervalRecord.PairType.EVERTED;
         }
-        return record.getType();
+        return type;
     }
 
+    /**
+     * Get the name of the sequence we are querying.
+     *
+     * @return sequence name
+     */
     public String getSequenceName() {
         return sequenceName;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     public void close() {
 
         if (samFileReader != null) {
