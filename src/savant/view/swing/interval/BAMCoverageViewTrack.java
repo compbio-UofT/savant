e95b205235ce521b33cfcb7877e3abb9ff67511f
Vanessa Williams <vanessa@fridgebuzz.com>
2010-04-22 14:09:24 +0000

    Fixed copyright notice.
    git-svn-id: https://svn2.assembla.com/svn/savant/Savant/trunk@496 096565f4-0d05-4fcd-8cbf-ff6523290f6f

--- src/savant/view/swing/interval/BAMCoverageViewTrack.java
+++ src/savant/view/swing/interval/BAMCoverageViewTrack.java
@@ -1,5 +1,5 @@
 /*
- *    Copyright 2009 Vanessa Williams
+ *    Copyright 2010 University of Toronto
  *
  *    Licensed under the Apache License, Version 2.0 (the "License");
  *    you may not use this file except in compliance with the License.
