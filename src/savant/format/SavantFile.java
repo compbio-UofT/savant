300313dc35685f9d578d3e48d0c09018f93a0fb0
Vanessa Williams <vanessa@fridgebuzz.com>
2010-05-06 18:30:37 +0000

    Added new constructor that throws IOException if file is not correct format.
    git-svn-id: https://svn2.assembla.com/svn/savant/Savant/trunk@603 096565f4-0d05-4fcd-8cbf-ff6523290f6f

--- src/savant/format/SavantFile.java
+++ src/savant/format/SavantFile.java
@@ -1,6 +1,17 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ *    Copyright 2010 University of Toronto
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
  */
 
 package savant.format;
@@ -30,6 +41,13 @@ public class SavantFile extends RandomAccessFile {
         this.fileTypeHeader = RAFUtils.readFileTypeHeader(this);
         this.fields = RAFUtils.readFieldsHeader(this);
         this.headerOffset = super.getFilePointer();
+    }
+
+    public SavantFile(String filename, FileType ft) throws IOException {
+        this(filename);
+        if (!fileTypeHeader.fileType.equals(ft)) {
+            throw new IOException("Wrong file type");
+        }        
     }
 
     public List<FieldType> getFields() {
