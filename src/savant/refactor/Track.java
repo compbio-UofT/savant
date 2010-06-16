ee050b699ab02c044c413fdc3d79d467d8eea4d0
Vanessa Williams <vanessa@fridgebuzz.com>
2010-06-16 20:11:54 +0000

    All methods with initial implementations.
    git-svn-id: https://svn2.assembla.com/svn/savant/Savant/branches/vw_refactor@739 096565f4-0d05-4fcd-8cbf-ff6523290f6f

--- src/savant/refactor/Track.java
+++ src/savant/refactor/Track.java
@@ -24,7 +24,12 @@
  */
 package savant.refactor;
 
-import java.util.List;
+import savant.model.Resolution;
+import savant.model.view.ColorScheme;
+import savant.model.view.Mode;
+import savant.util.Range;
+
+import java.util.*;
 
 /**
  * Class to represent a particular data view. It may use many DataSources and many Renderers for each.
@@ -36,8 +41,247 @@ import java.util.List;
  */
 public abstract class Track {
 
-    private VizPane vizPane;
     private TrackConfigurator trackConfigurator;
     private Grapher grapher;
-    private List<DataSource> dataSources;
+    private List<DataSource> dataSources = new ArrayList<DataSource>();
+
+    private Map<DataSource, List<Renderer>> renderMap = new HashMap<DataSource, List<Renderer>>();
+
+    private boolean locked;
+    private Range lockedRange;
+    private boolean panning;
+    private boolean zooming;
+    private boolean dragging;
+
+    private ColorScheme colorScheme;
+
+    private String name;
+
+    // currently loaded data
+    private RefSeq loadedSequence;
+    private Range loadedRange;
+    private Resolution loadedResolution;
+    private Map<DataSource, List> dataMap;
+
+    /**
+     * Add a new data source to the track
+     * @param dataSource the source to add
+     */
+    public void addDataSource(DataSource dataSource) {
+        dataSources.add(dataSource);
+    }
+
+    /**
+     * Add a data source and associated renderer to the track
+     * @param dataSource the DataSource
+     * @param renderer the Renderer
+     */
+    public void addDataSource(DataSource dataSource, Renderer renderer) {
+        dataSources.add(dataSource);
+        List<Renderer> renderers;
+        if (renderMap.containsKey(dataSource)) {
+            renderers = renderMap.get(dataSource);
+        }
+        else {
+            renderers = new ArrayList<Renderer>();
+            renderMap.put(dataSource, renderers);
+        }
+        renderers.add(renderer);
+
+    }
+
+    /**
+     * Add a data source and a list of associated renderers to the track
+     * @param dataSource the DataSource
+     * @param rendererList a List of Renderers
+     */
+    public void addDataSource(DataSource dataSource, List<Renderer> rendererList) {
+        dataSources.add(dataSource);
+        List<Renderer> renderers;
+        if (renderMap.containsKey(dataSource)) {
+            renderers = renderMap.get(dataSource);
+        }
+        else {
+            renderers = new ArrayList<Renderer>();
+            renderMap.put(dataSource, renderers);
+        }
+        renderers.addAll(rendererList);
+
+    }
+
+    /**
+     * Add a renderer to the list of renderers associated with an existing DataSource
+     * @param dataSource DataSource already associated with the Track
+     * @param renderer an additional Renderer
+     */
+    public void addRenderer(DataSource dataSource, Renderer renderer) {
+
+        if (dataSources.contains(dataSource)) {
+            List<Renderer> renderers;
+            if (renderMap.containsKey(dataSource)) {
+                renderers = renderMap.get(dataSource);
+            }
+            else {
+                renderers = new ArrayList<Renderer>();
+                renderMap.put(dataSource, renderers);
+            }
+            renderers.add(renderer);
+        }
+    }
+
+    /**
+     * Get all Renderers for a given DataSource
+     * @param dataSource the DataSource to return renderers for
+     * @return List of Renderers for the given DataSource; an empty list if no Renderers available
+     */
+    public List<Renderer> getRenderers(DataSource dataSource) {
+        if (renderMap.containsKey(dataSource))
+            return renderMap.get(dataSource);
+        else {
+            List<Renderer> l = Collections.EMPTY_LIST;
+            return l;
+        }           
+    }
+
+    /**
+     * Get all Renderers associated with this Track, regardless of DataSource
+     * @return a (possibly) empty List of renderers
+     */
+    public List<Renderer> getRenderers() {
+        List<Renderer> results = new ArrayList<Renderer>();
+        for (DataSource source: renderMap.keySet()) {
+            List<Renderer> renderersForSource = renderMap.get(source);
+            if (renderersForSource != null) {
+                results.addAll(renderersForSource);
+            }
+        }
+        return results;
+    }
+
+    /**
+     * Render all data sources associated with this track
+     * @param canvas some drawing surface, e.g awt.Graphics
+     * @param sequence the sequence to use
+     * @param range the range to draw
+     * @param resolution the resolution at which to draw
+     */
+    public abstract void drawDataSources(Object canvas, RefSeq sequence, Range range, Resolution resolution);
+
+    /**
+     * Render a legend for this track
+     * @param canvas  some drawing surface, e.g awt.Graphics
+     */
+    public abstract void drawLegend(Object canvas);
+
+    /**
+     * Render a background for this track
+     *
+     * @param canvas some drawing surface, e.g awt.Graphics
+     * @param sequence the sequence to use
+     * @param range the range to draw
+     * @param resolution the resolution at which to draw
+     */
+    public abstract void drawBackground(Object canvas, RefSeq sequence, Range range, Resolution resolution);
+
+    /**
+     * Adjust to the fact that some renderer has changed its mode, e.g. change the mode of another renderer.
+     * @param dataSource the DataSource whose Renderer has changed mode
+     * @param renderer the Renderer whose Mode has changed
+     * @param mode the new Mode
+     */
+    public void switchMode(DataSource dataSource, Renderer renderer, Mode mode) {
+        // default does nothing, subclasses may override
+    }
+
+    /**
+     * Load a data set in preparation for drawing.
+     *
+     * @param sequence the sequence to use
+     * @param range the range to fetch
+     * @param resolution the resolution at which to get the data
+     */
+    public void preloadData(RefSeq sequence, Range range, Resolution resolution) {
+
+        if (!sequence.equals(this.loadedSequence) || !range.equals(this.loadedRange) || !resolution.equals(this.loadedResolution)) {
+            for (DataSource source: dataSources) {
+                dataMap.put(source, source.getRange(sequence, range, resolution));
+            }
+        }
+    }
+
+    public Range getLockedRange() {
+        return this.lockedRange;    
+    }
+
+    public TrackConfigurator getTrackConfigurator() {
+        return trackConfigurator;
+    }
+
+    public void setTrackConfigurator(TrackConfigurator trackConfigurator) {
+        this.trackConfigurator = trackConfigurator;
+    }
+
+    public Grapher getGrapher() {
+        return grapher;
+    }
+
+    public void setGrapher(Grapher grapher) {
+        this.grapher = grapher;
+    }
+
+    public List<DataSource> getDataSources() {
+        return dataSources;
+    }
+
+    public void setDataSources(List<DataSource> dataSources) {
+        this.dataSources = dataSources;
+    }
+
+    public boolean isLocked() {
+        return locked;
+    }
+
+    public void setLocked(boolean locked) {
+        this.locked = locked;
+    }
+
+    public boolean isPanning() {
+        return panning;
+    }
+
+    public void setPanning(boolean panning) {
+        this.panning = panning;
+    }
+
+    public boolean isZooming() {
+        return zooming;
+    }
+
+    public void setZooming(boolean zooming) {
+        this.zooming = zooming;
+    }
+
+    public boolean isDragging() {
+        return dragging;
+    }
+
+    public void setDragging(boolean dragging) {
+        this.dragging = dragging;
+    }
+
+    public ColorScheme getColorScheme() {
+        return colorScheme;
+    }
+
+    public void setColorScheme(ColorScheme colorScheme) {
+        this.colorScheme = colorScheme;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
