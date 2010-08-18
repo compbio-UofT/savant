83f3fd94ae83fc6e3d5da46b0b7eecda7488ef61
Andrew Brook <abrook@@cs.toronto.edu>
2010-08-18 19:23:30 +0000

    Added Comparable to ContinuousRecord
    git-svn-id: https://svn2.assembla.com/svn/savant/Savant/trunk@947 096565f4-0d05-4fcd-8cbf-ff6523290f6f

--- src/savant/model/ContinuousRecord.java
+++ src/savant/model/ContinuousRecord.java
@@ -25,7 +25,7 @@ package savant.model;
  * Class to contain a value and the position at which that value obtains.
  * @author vwilliams
  */
-public class ContinuousRecord implements Record {
+public class ContinuousRecord implements Record, Comparable {
 
     private String reference;
     private Continuous value;
@@ -91,6 +91,33 @@ public class ContinuousRecord implements Record {
         sb.append(", position=").append(position);
         sb.append('}');
         return sb.toString();
+    }
+
+    @Override
+    public int compareTo(Object o) {
+        ContinuousRecord that = (ContinuousRecord) o;
+
+        //compare ref
+        if(!this.getReference().equals(that.getReference())){
+            String a1 = this.getReference();
+            String a2 = that.getReference();
+            for(int i = 0; i < Math.min(a1.length(), a2.length()); i++){
+                if((int)a1.charAt(i) < (int)a2.charAt(i)) return -1;
+                else if ((int)a1.charAt(i) > (int)a2.charAt(i)) return 1;
+            }
+            if(a1.length() < a2.length()) return -1;
+            if(a1.length() > a2.length()) return 1;
+        }
+
+        //compare position
+        if(this.getPosition() == that.getPosition()){
+            return 0;
+        } else if (this.getPosition() < that.getPosition()){
+            return -1;
+        } else {
+            return 1;
+        }
+        
     }
 
 }
