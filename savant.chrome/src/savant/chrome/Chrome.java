ab2ab3c2080829f72b58caf111bbc11ae352e191
Marc Fiume <mfiume@cs.toronto.edu>
2010-12-09 18:01:39 +0000

    Chrome plugin stump
    git-svn-id: https://svn2.assembla.com/svn/savant/Savant/trunk@1310 096565f4-0d05-4fcd-8cbf-ff6523290f6f

---
+++ savant.chrome/src/savant/chrome/Chrome.java
@@ -0,0 +1,53 @@
+/*
+ * DataTab.java
+ * Created on Feb 25, 2010
+ *
+ *
+ *    Copyright 2010 University of Toronto
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package savant.chrome;
+
+import javax.swing.JButton;
+import javax.swing.JPanel;
+
+import savant.plugin.PluginAdapter;
+import savant.plugin.SavantPanelPlugin;
+
+public class Chrome extends SavantPanelPlugin {
+
+    @Override
+    public void init(JPanel tablePanel, PluginAdapter pluginAdapter) {
+        JButton b = new JButton("Hello world");
+        tablePanel.add(b);
+    }
+
+
+    @Override
+    protected void doStart() throws Exception {
+
+    }
+
+    @Override
+    protected void doStop() throws Exception {
+
+    }
+
+    @Override
+    public String getTitle() {
+        return "Chrome";
+    }
+
+}
