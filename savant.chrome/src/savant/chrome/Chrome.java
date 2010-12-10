9264d5a713b958970463c4481bf012c6b216962b
Marc Fiume <mfiume@cs.toronto.edu>
2010-12-10 00:03:26 +0000

    Chrome stub2
    git-svn-id: https://svn2.assembla.com/svn/savant/Savant/trunk@1312 096565f4-0d05-4fcd-8cbf-ff6523290f6f

--- savant.chrome/src/savant/chrome/Chrome.java
+++ savant.chrome/src/savant/chrome/Chrome.java
@@ -17,32 +17,53 @@
  *    See the License for the specific language governing permissions and
  *    limitations under the License.
  */
-
 package savant.chrome;
 
+import java.awt.BorderLayout;
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.JButton;
 import javax.swing.JPanel;
+import org.biojava.bio.chromatogram.Chromatogram;
+import org.biojava.bio.chromatogram.ChromatogramFactory;
+import org.biojava.bio.chromatogram.UnsupportedChromatogramFormatException;
+import savant.api.util.DialogUtils;
+import savant.api.util.GenomeUtils;
+import savant.controller.event.RangeChangeCompletedListener;
+import savant.controller.event.RangeChangedEvent;
 
 import savant.plugin.PluginAdapter;
 import savant.plugin.SavantPanelPlugin;
 
-public class Chrome extends SavantPanelPlugin {
+public class Chrome extends SavantPanelPlugin implements RangeChangeCompletedListener {
+
+    Chromatogram c = null;
 
     @Override
     public void init(JPanel tablePanel, PluginAdapter pluginAdapter) {
+
+        savant.api.util.NavigationUtils.addRangeChangeListener(this);
         JButton b = new JButton("Hello world");
         tablePanel.add(b);
+        try {
+            c = ChromatogramFactory.create(new File("C:\\91548-HEXBF.ab1"));
+        } catch (IOException ex) {
+            DialogUtils.displayException("Uh Oh..", "Error reading chromatogram", ex);
+        } catch (UnsupportedChromatogramFormatException ex) {
+            DialogUtils.displayException("Uh Oh..", "Error reading chromatogram", ex);
+        } catch (Error ex) {
+            DialogUtils.displayException("Uh Oh..", "Error reading chromatogram", ex);
+        }
     }
-
 
     @Override
     protected void doStart() throws Exception {
-
     }
 
     @Override
     protected void doStop() throws Exception {
-
     }
 
     @Override
@@ -50,4 +71,21 @@ public class Chrome extends SavantPanelPlugin {
         return "Chrome";
     }
 
+    JPanel canvas = null;
+
+    @Override
+    public void rangeChangeCompletedReceived(RangeChangedEvent event) {
+        if (canvas == null) {
+            if (GenomeUtils.isGenomeLoaded() && GenomeUtils.getGenome().isSequenceSet()) {
+                canvas = GenomeUtils.getGenome().getViewTrack().getLayerCanvas();
+                canvas.setLayout(new BorderLayout());
+                canvas.add(new ChromePanel(c), BorderLayout.CENTER);
+            }
+        }
+
+        if (canvas != null) {
+            canvas.repaint();
+            canvas.revalidate();
+        }
+    }
 }
---
+++ savant.chrome/src/savant/chrome/ChromePanel.java
@@ -0,0 +1,37 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package savant.chrome;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import javax.swing.JPanel;
+import org.biojava.bio.chromatogram.Chromatogram;
+import org.biojava.bio.chromatogram.graphic.ChromatogramGraphic;
+
+/**
+ *
+ * @author mfiume
+ */
+class ChromePanel extends JPanel {
+
+    Chromatogram c;
+
+    public ChromePanel(Chromatogram c) {
+        this.c = c;
+        this.setOpaque(false);
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        ChromatogramGraphic cg = (new ChromatogramGraphic(c));
+        cg.setHeight(this.getHeight());
+        cg.setWidth(this.getWidth()*10);
+        cg.drawTo((Graphics2D) g);
+    }
+}
