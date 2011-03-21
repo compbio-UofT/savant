c5b2393e2a136bcb82599b52e00cd756630fdb13
Eric Smith <eric.smith@utoronto.ca>
2011-03-21 21:31:02 +0000

    DataFormatterThread now catches Throwable rather than Exception so that we can (sometimes) report OutOfMemory errors.
    git-svn-id: https://svn2.assembla.com/svn/savant/Savant/trunk@1448 096565f4-0d05-4fcd-8cbf-ff6523290f6f

--- src/savant/view/dialog/FormatFrame.java
+++ src/savant/view/dialog/FormatFrame.java
@@ -1,20 +1,26 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ *    Copyright 2010-2011 University of Toronto
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
  */
 
-/*
- * FormatFrame.java
- *
- * Created on Oct 4, 2010, 7:14:37 PM
- */
 package savant.view.dialog;
 
+import java.io.File;
 import javax.swing.JDialog;
 import javax.swing.JOptionPane;
 
 import com.jidesoft.dialog.JideOptionPane;
-import java.io.File;
 
 import savant.format.DataFormatter;
 import savant.format.DataFormatterThread;
@@ -264,7 +270,7 @@ public class FormatFrame extends javax.swing.JFrame implements FormatProgressLis
     private javax.swing.JProgressBar progress_current;
     // End of variables declaration//GEN-END:variables
 
-    public void notifyOfTermination(boolean wasFormatSuccessful, Exception e) {
+    public void notifyOfTermination(boolean wasFormatSuccessful, Throwable e) {
         if (wasFormatSuccessful) {
             int result = JOptionPane.showConfirmDialog(this, "Format successful. Open track now?", "Format Successful", JOptionPane.YES_NO_OPTION);
             this.setVisible(false);
