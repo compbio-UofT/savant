7d1aea382d22bfba9155ece179a90d384955887c
Eric Smith <eric.smith@utoronto.ca>
2011-06-28 14:32:51 +0000

    Prepping for 1.5.0 build (again).
    git-svn-id: https://svn2.assembla.com/svn/savant/Savant/trunk@1839 096565f4-0d05-4fcd-8cbf-ff6523290f6f

--- src/savant/settings/BrowserSettings.java
+++ src/savant/settings/BrowserSettings.java
@@ -55,7 +55,7 @@ public class BrowserSettings {
 
 
     public static final String VERSION = "1.5.0";
-    public static final String BUILD = "";
+    public static String BUILD = "";
 
 
     public static boolean getCheckVersionOnStartup() {
