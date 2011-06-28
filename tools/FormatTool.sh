d6794798ba6812a88223a4671370d92f7500424a
Eric Smith <eric.smith@utoronto.ca>
2011-06-28 15:27:35 +0000

    Netbeans SVN client generated message: create a new folder for the copy: 1.5.0 release.
    git-svn-id: https://svn2.assembla.com/svn/savant/Savant/branches/release-1.5.0@1842 096565f4-0d05-4fcd-8cbf-ff6523290f6f

--- tools/FormatTool.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-JAVAROOT=.
-if [[ `uname` == *Darwin* ]]; then
-  JAVAROOT=/Applications/Savant.app/Contents/Resources/Java
-fi
-CLASSPATH=$JAVAROOT/Savant.jar:$JAVAROOT/commons-logging-1.1.1.jar:$JAVAROOT/sam-1.31.jar
-java -cp $CLASSPATH savant.tools.FormatTool $*
