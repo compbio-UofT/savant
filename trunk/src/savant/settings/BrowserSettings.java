9deabf210b391b029f9935e7c58c66256cc3828b
Eric Smith <eric.smith@utoronto.ca>
2011-08-08 15:57:38 +0000

    Removed "beta" flag in preparation for 1.5.1 official release.
    git-svn-id: https://svn2.assembla.com/svn/savant/Savant/tags/release-1.5.1-beta@2143 096565f4-0d05-4fcd-8cbf-ff6523290f6f

--- trunk/src/savant/settings/BrowserSettings.java
+++ trunk/src/savant/settings/BrowserSettings.java
@@ -58,7 +58,7 @@ public class BrowserSettings {
     public static final URL SAFE_URL = NetworkUtils.getKnownGoodURL(URL, "safe/savantsafe.php");
 
     public static final String VERSION = "1.5.1";
-    public static String BUILD = "beta";
+    public static String BUILD = "";
 
     /**
      * Is this version a beta release?
