86dee7efc4db0777d22457ee1f17310a67e7c947
Eric Smith <eric.smith@utoronto.ca>
2011-09-23 19:45:07 +0000

    Initial check-in of Diff plugin (part of 1.6.0 SDK).
    git-svn-id: https://svn2.assembla.com/svn/savant/Savant/trunk@2375 096565f4-0d05-4fcd-8cbf-ff6523290f6f

---
+++ savant.diff/src/savant/diff/DiffDataSource.java
@@ -0,0 +1,249 @@
+/*
+ *    Copyright 2011 University of Toronto
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package savant.diff;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import savant.api.adapter.BookmarkAdapter;
+import savant.api.adapter.DataSourceAdapter;
+import savant.api.adapter.RangeAdapter;
+import savant.api.adapter.TrackAdapter;
+import savant.api.util.TrackUtils;
+import savant.data.types.GenericContinuousRecord;
+import savant.file.DataFormat;
+import savant.util.MiscUtils;
+import savant.util.Resolution;
+
+/**
+ * The actual data-source implemented by the Diff plugin.  Takes two existing continuous data-sources
+ * and creates a third data-source based on the difference between them.
+ *
+ * @author tarkvara
+ */
+public class DiffDataSource implements DataSourceAdapter<GenericContinuousRecord> {
+    private static final Log LOG = LogFactory.getLog(DiffDataSource.class);
+
+    /** Our two input data-sources. */
+    DataSourceAdapter<? extends GenericContinuousRecord> inputA, inputB;
+    
+    private URI uri;
+
+    /**
+     * Construct a data-source for the given diff:// URI.
+     */
+    public DiffDataSource(URI uri) throws URISyntaxException {
+        this.uri = uri;
+    }
+
+    /**
+     * Set of references is whatever's common to both input tracks.
+     */
+    @Override
+    public Set<String> getReferenceNames() {
+        Set<String> result = new HashSet<String>();
+        if (inputsAttached()) {
+            // In a perfect world, we could just intersect the two sets and be done.
+            // In practice, some tracks will store chromosome names like "chr1", "chr2", etc.,
+            // while others will use "1", "2".  We reconcile these as best we can.
+            Set<String> aRefs = inputA.getReferenceNames();
+            Set<String> bRefs = inputB.getReferenceNames();
+            for (String a: aRefs) {
+                String goodA = MiscUtils.homogenizeSequence(a);     // Reduce "chr1" to "1".
+                String match = null;
+                for (String b: bRefs) {
+                    String goodB = MiscUtils.homogenizeSequence(b); // Reduce to "1" before comparing.
+                    if (goodA.equals(goodB)) {
+                        match = b;
+                        break;
+                    }
+                }
+                if (match != null) {
+                    result.add(goodA);
+                    bRefs.remove(match);
+                }
+            }
+        }
+        return result;
+    }
+
+    /**
+     * The number of records we return will be based on the first input.  In some cases,
+     * the second input may not happen to have a point at the expected location, so
+     * we may have to interpolate.
+     * 
+     * The data-sources are expected to return records in order, so we can count on that
+     * to simplify our task.
+     */
+    @Override
+    public List<GenericContinuousRecord> getRecords(String ref, RangeAdapter range, Resolution res) throws IOException {
+        List<GenericContinuousRecord> result = null;
+        if (inputsAttached()) {
+            List<? extends GenericContinuousRecord> aRecords = inputA.getRecords(ref, range, res);
+            List<? extends GenericContinuousRecord> bRecords = inputB.getRecords(ref, range, res);
+
+            result = new ArrayList<GenericContinuousRecord>(aRecords.size());
+            int j = 0;
+            GenericContinuousRecord recB = bRecords.get(j);
+            for (int i = 0; i < aRecords.size(); i++) {
+                GenericContinuousRecord recA = aRecords.get(i);
+                int pos = recA.getPosition();
+
+                // Figure out which record in B corresponds to A.
+                while (recB.getPosition() < pos && j + 1 < bRecords.size()) {
+                    j++;
+                    recB = bRecords.get(j);
+                }
+                
+                // For the purposes of this demonstration, we'll treat NaNs as zero.
+                float value = recA.getValue();
+                if (Float.isNaN(value)) {
+                    value = 0.0f;
+                }
+                value -= interpolate(bRecords, j, pos);
+                result.add(GenericContinuousRecord.valueOf(ref, pos, value));
+            }
+        }
+        return result;
+    }
+
+    /**
+     * Because the two input data-sources may be returning differing numbers of records,
+     * we may have to interpolate.  Arbitrarily, we decide that inputA provides the
+     * bench-mark and inputB gets interpolated.
+     */
+    private float interpolate(List<? extends GenericContinuousRecord> bRecords, int j, int pos) {
+        float result = 0.0f;
+        if (j < bRecords.size()) {
+            GenericContinuousRecord recB = bRecords.get(j);
+
+            if (recB.getPosition() == pos || j == 0) {
+                // Simple case.  We have a data-point at the exact position.
+                result = recB.getValue();
+            } else if (recB.getPosition() > pos) {
+                // If we got here, recB is further on in the chromosome than pos, so we need to interpolate with the preceding data-point.
+                GenericContinuousRecord prevRecB = bRecords.get(j - 1);
+                float weight = (float)(pos - prevRecB.getPosition()) / (recB.getPosition() - prevRecB.getPosition());
+                result = prevRecB.getValue() * weight + recB.getValue() * (1.0f - weight);
+            }
+        }
+        return Float.isNaN(result) ? 0.0f : result;
+    }
+
+    /**
+     * Get the URI corresponding to the difference between our input tracks.
+     * @return
+     */
+    @Override
+    public URI getURI() {
+        return uri;
+    }
+
+    /**
+     * @return
+     */
+    @Override
+    public String getName() {
+        return uri.toString();
+    }
+
+    /**
+     * No extra clean-up to be done.
+     */
+    @Override
+    public void close() {
+    }
+
+    /**
+     * @return <code>CONTINUOUS_GENERIC</code>
+     */
+    @Override
+    public DataFormat getDataFormat() {
+        return DataFormat.CONTINUOUS_GENERIC;
+    }
+
+    /**
+     * @return "Reference", "Position", "Value"
+     */
+    @Override
+    public String[] getColumnNames() {
+        return GenericContinuousRecord.COLUMN_NAMES;
+    }
+
+    /**
+     * A do-nothing stub because continuous tracks don't have any strings to look up.
+     */
+    @Override
+    public void loadDictionary() {
+    }
+
+    /**
+     * A do-nothing stub because continuous tracks don't have any strings to look up.
+     */
+    @Override
+    public List<BookmarkAdapter> lookup(String string) {
+        return null;
+    }
+
+    /**
+     * Utility method to calculate the URI which will be used to specify the difference between the two given tracks.
+     */
+    public static URI getDiffURI(TrackAdapter trackA, TrackAdapter trackB) throws URISyntaxException {
+        return new URI("diff://(" + trackA.getDataSource().getURI() + ";" + trackB.getDataSource().getURI() + ")");
+    }
+
+    /**
+     * Search through our existing tracks to find the ones which match our input URIs.
+     * Because we can't rely on Savant loading the tracks in any known order, we may have
+     * to call this method repeatedly before the inputs get hooked up.
+     */
+    private boolean inputsAttached() {
+        if (inputA != null && inputB != null) {
+            return true;
+        }
+        try {
+            String uriString = uri.getRawSchemeSpecificPart().substring(3);     // Trim off the initial "diff://("
+            uriString = uriString.substring(0, uriString.length() - 1);         // Trim off the final ")"
+            int delimiterPos = uriString.indexOf(';');
+            if (delimiterPos > 0) {
+                URI uriA = new URI(uriString.substring(0, delimiterPos));
+                URI uriB = new URI(uriString.substring(delimiterPos + 1));
+
+                TrackAdapter[] availableTracks = TrackUtils.getTracks(DataFormat.CONTINUOUS_GENERIC);
+                for (TrackAdapter t: availableTracks) {
+                    URI u = t.getDataSource().getURI();
+                    if (u.equals(uriA)) {
+                        inputA = t.getDataSource();
+                    } else if (u.equals(uriB)) {
+                        inputB = t.getDataSource();
+                    }
+                }
+            }
+        } catch (URISyntaxException x) {
+            LOG.error(String.format("Unable to parse %s as a valid URI.", x));
+        }
+        return inputA != null && inputB != null;
+    }
+}
---
+++ savant.diff/src/savant/diff/DiffPlugin.java
@@ -0,0 +1,77 @@
+/*
+ *    Copyright 2011 University of Toronto
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package savant.diff;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import savant.api.adapter.DataSourceAdapter;
+import savant.api.adapter.TrackAdapter;
+import savant.api.util.DialogUtils;
+import savant.api.util.TrackUtils;
+import savant.file.DataFormat;
+import savant.plugin.SavantDataSourcePlugin;
+
+
+/**
+ * Sample DataSource plugin created as an example for the Savant 1.6.0 SDK.
+ *
+ * @author tarkvara
+ */
+public class DiffPlugin extends SavantDataSourcePlugin {
+
+    @Override
+    public void init() {
+    }
+
+    @Override
+    public String getTitle() {
+        return "Diff Plugin";
+    }
+
+    /**
+     * Invoked when the user wants to define a fresh Diff track from two existing tracks.
+     */
+    @Override
+    public DataSourceAdapter getDataSource() throws URISyntaxException {
+        TrackAdapter[] availableTracks = TrackUtils.getTracks(DataFormat.CONTINUOUS_GENERIC);
+        if (availableTracks.length >= 2) {
+            SourceDialog dlg = new SourceDialog(availableTracks);
+            dlg.setVisible(true);
+            if (dlg.result != null) {
+                return new DiffDataSource(dlg.result);
+            }
+        } else {
+            DialogUtils.displayMessage("Sorry", "The Diff plugin requires at least two continuous tracks to serve as sources.");
+        }
+        return null;
+    }
+
+    /**
+     * Invoked when Savant is constructing a Diff track from an existing URI (e.g. when
+     * opening a project which has an existing diff:// track).
+     */
+    @Override
+    public DataSourceAdapter getDataSource(URI uri) {
+        try {
+            if (uri.getScheme().equals("diff")) {
+                return new DiffDataSource(uri);
+            }
+        } catch (URISyntaxException ignored) {
+        }
+        return null;
+    }
+}
