a61393174df413bec2f438feff924f492858efea
Eric Smith <eric.smith@utoronto.ca>
2012-02-01 22:39:18 +0000

    Integrating latest BigWig support. Tabix and BigWig libraries now part of Savant.jar.
    git-svn-id: https://svn2.assembla.com/svn/savant/Savant/trunk@3144 096565f4-0d05-4fcd-8cbf-ff6523290f6f

--- src/savant/data/sources/BigWigDataSource.java
+++ src/savant/data/sources/BigWigDataSource.java
@@ -69,10 +69,8 @@ public class BigWigDataSource extends DataSource<GenericContinuousRecord> {
     @Override
     public List<GenericContinuousRecord> getRecords(String ref, RangeAdapter range, Resolution resolution, RecordFilterAdapter filt) throws IOException, InterruptedException {
         List<GenericContinuousRecord> result = new ArrayList<GenericContinuousRecord>();
-        int chromID = bbReader.getChromosomeID(ref);
-        RPChromosomeRegion region = new RPChromosomeRegion(chromID, range.getFrom(), chromID, range.getTo());
         try {
-            BigWigIterator wigIterator = bbReader.getBigWigIterator(region, false);
+            BigWigIterator wigIterator = bbReader.getBigWigIterator(ref, range.getFrom(), ref, range.getTo(), false);
             int nextPos = range.getFrom();
             int rangeEnd = range.getTo();
             while (wigIterator.hasNext()) {
